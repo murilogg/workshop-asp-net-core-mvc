d9ce3a387dd993e9d572fb88e55d0337de795ffe
Murilo Gomes <murilo29@hotmail.com.br>
2019-12-16 09:30:11 -0400

    First controller and Razor pages tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web MVC App from C# Course.";
+            ViewData["Email"] = "murilo@gmail.com";
+            ViewData["Professor"] = "Murilo santos";
 
             return View();
         }
