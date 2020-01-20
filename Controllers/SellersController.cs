1a19b67c30af634f9f44e210d4a0de6f1fb515b9
Murilo Gomes <murilo29@hotmail.com.br>
2020-01-20 17:09:18 -0400

    SellerFormViewModel and Department select component

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -14,11 +15,13 @@ namespace SalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-
-        // Construct
-        public SellersController(SellerService sellerService)
+        private readonly DepartmentService _departmentService;
+        
+        // Construct]
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         // Method Find All 
@@ -30,7 +33,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
